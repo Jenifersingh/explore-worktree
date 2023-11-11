3a72c7baa3d7c30b2654f5e4c7b6a9e3640265f5
Y S Jenifer Singh <jenifersingh@kissflow.com>
2023-11-11 14:23:01 +0530

    for work tree testing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import VirtualList from "react-tiny-virtual-list";
+import create from "zustand";
+
+import logo from "./logo.svg";
+import "./App.css";
+
+const useStore = create((set) => ({
+  bears: 10,
+  increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
+  removeAllBears: () => set({ bears: 0 }),
+}));
 
 function App() {
+  const { bears } = useStore((state) => ({
+    bears: state.bears,
+  }));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <VirtualList
+      width={300}
+      height={300}
+      itemCount={bears ? 30 : 8}
+      itemSize={30}
+      style={{ border: "1px solid" }}
+      renderItem={({ index, style }) => (
+        <div key={index} style={style}>
+          Row: #{index}
+        </div>
+      )}
+    />
   );
 }
 
